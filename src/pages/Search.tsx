304d548cd988e0884301d50e8d9c83df2e15a913
Sergio Acosta <sergioacostaquintana@gmail.com>
2023-09-27 12:33:33 +0100

    style error

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -4,7 +4,7 @@ function Search() {
   return (
     <div className="h-full w-full relative">
       <div className="absolute top-0 left-0 w-full z-10">
-        <PageHeader opacity={""} />
+        <PageHeader />
       </div>
     </div>
   );
